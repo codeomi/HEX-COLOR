f8cd88898053a3f3c9cd0d35ee5a751f49cb856b
Omkar Kudale <omkarkudale007@gmail.com>
2022-09-17 00:49:33 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const colorBtn = document.getElementById("chnge-colo-btn")
+let hexId = document.getElementById("hex-id")
+const backgroundColor = document.getElementById("color")
+
+colorBtn.addEventListener("click", function() {
+    let colorId =   '#' + Math.random().toString(16).substring(2, 8);
+    hexId.textContent = ` HEX id:- ${colorId}`
+    document.body.style.backgroundColor = colorId;
+    // colorBtn.style.backgroundColor = colorId
+    console.log(opaqueColor)
+})
+
