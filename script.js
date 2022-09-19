54160f2dfd19629eea999f8c1c729802dbe6015a
Omkar Kudale <omkarkudale007@gmail.com>
2022-09-19 17:24:22 +0530

    You can also change color using enter button

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const colorBtn = document.getElementById("chnge-colo-btn")
 let hexId = document.getElementById("hex-id")
 const backgroundColor = document.getElementById("color")
 
-colorBtn.addEventListener("click", function() {
+colorBtn.addEventListener("enter", function() {
     let colorId =   '#' + Math.random().toString(16).substring(2, 8);
     hexId.textContent = ` HEX id:- ${colorId}`
     document.body.style.backgroundColor = colorId;
